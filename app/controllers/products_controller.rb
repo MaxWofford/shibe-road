823dd34ac2bdd43ae1bfccee505a13188db7ef13
Max Wofford <max.speed.wofford@gmail.com>
2014-10-23 23:01:22 -0600

    strong params on products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,9 @@
-class ProductsController < InheritedResources::Base
+class ProductsController < ApplicationController
+  inherit_resources
+
+  private
+
+  def product_params
+    params.require(:product).permit(:name, :description, :public_address)
+  end
 end
